de3df3907f835c7d380b4926dafd5b785979f2eb
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-06-30 12:39:24 +0700

    menetapakan nilai ke bidang data ke node baru

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -31,4 +31,5 @@ public:
 	void insert(string element) //Insert a node in the binaryy search tree
 	{
 		Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
+		newNode->info = element; //Assign value to the data field of the new node
 		
